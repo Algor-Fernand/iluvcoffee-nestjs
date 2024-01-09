65ad9eca0a1300eafa79928e1682c9461a5e1cf7
Algor-Fernand <algorfernand014@gmail.com>
2024-01-09 10:19:32 +0200

    feat: user management nest

---
+++ src/Module/user.module.ts
@@ -0,0 +1,8 @@
+// user.module.ts
+import { Module } from '@nestjs/common';
+import { UserController } from '../controllers/user.controller';
+
+@Module({
+  controllers: [UserController],
+})
+export class UserModule {}
