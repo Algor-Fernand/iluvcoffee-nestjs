429d9d38f68a4f1ce76fc8bd310cf550d0db63e8
Algor-Fernand <algorfernand014@gmail.com>
2024-01-09 10:22:32 +0200

    fix:delete user module

--- src/Module/user.module.ts
+++
@@ -1,8 +0,0 @@
-// user.module.ts
-import { Module } from '@nestjs/common';
-import { UserController } from '../controllers/user.controller';
-
-@Module({
-  controllers: [UserController],
-})
-export class UserModule {}
