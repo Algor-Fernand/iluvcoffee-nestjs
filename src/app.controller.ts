91c4eaab3ff056d43bad5054d25bc1a665a43d0a
Algor-Fernand <algorfernand014@gmail.com>
2024-01-09 10:22:32 +0200

    fix: controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
-// import { AppService } from './app.service';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor() {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
-    return 'hi'; // return this.appService.getHello();
+    return this.appService.getHello();
   }
 }
